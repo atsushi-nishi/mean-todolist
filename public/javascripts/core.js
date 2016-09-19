63a1dfaedb7478b7c0f49714448298239f4fc8bd
Atsushi Nishi <atsushi.nishi@Trickster14s-MacBook-Air.local>
2016-09-19 19:13:32 +0900

    forget add files

---
+++ public/javascripts/core.js
@@ -0,0 +1,63 @@
+// public/core.js
+var myModule = angular.module('myModule', []);
+
+myModule.controller('mainCtrl', [
+  '$scope',
+  '$http',
+  function($scope, $http){
+    $scope.test = 'Hello world!';
+    console.log($scope.test);
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/users')
+        .success(function(data) {
+            $scope.users = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+}]);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+    console.log('mainController');
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/users')
+        .success(function(data) {
+            $scope.users = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+/*
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+*/
+
+}
